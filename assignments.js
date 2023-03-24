597ba9874caec45028196c04f3346f9289fe1aac
rizkirisjad <rizkirisjad@gmail.com>
2023-03-24 12:49:16 +0700

    menambah materi baru

--- assignments.js
+++ assignments.js
@@ -1,5 +1,30 @@
 // Lecture: Values and Variables
-let country = "Indonesia";
-let continent = "Asia";
-let population = "270.20 million ";
-console.log(country, continent, population);
+let country = 'Indonesia';
+let continent = 'Asia';
+let population = 281358622;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// LECTURE: Data Types
+const isIsland = true;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+//LECTURE: let, const and var
+language = 'Bahasa Indonesia';
+
+//LECTURE: Basic Operators
+let halfCountry = population / 2;
+population++;
+console.log(halfCountry, population);
+
+let finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+let avgPopulation = 33000000;
+console.log(population < avgPopulation);
+const description =
+  'Indonesia is in Asia, and its 281,35 million people speak bahasa indonesia';
