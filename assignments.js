650c1fa86e192517904046852eb116fee86fe131
rizkirisjad <rizkirisjad@gmail.com>
2023-03-27 22:19:50 +0700

    menambah materi baru

--- assignments.js
+++ assignments.js
@@ -37,6 +37,20 @@ const description =
   language;
 console.log(description);
 
-//LECTURE: Strings and Template Literals
+// LECTURE: Strings and Template Literals
 const newDescription = `${country} is in ${continent}, and its 281,35 million people speak ${language}`;
 console.log(newDescription);
+
+// LECTURE: Taking Decisions: if / else Statements
+if (population > 33000000) {
+  console.log(`population of ${country} is above average`);
+} else {
+  `population of ${country} is 22 million below average`;
+}
+
+// LECTURE: Type Conversion and Coercion
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
