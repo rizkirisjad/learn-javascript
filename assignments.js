290247f102bf11533c3a14586f2b67369472ee9c
rizkirisjad <rizkirisjad@gmail.com>
2023-03-28 20:03:49 +0700

    menambah materi baru

--- assignments.js
+++ assignments.js
@@ -54,3 +54,15 @@ console.log('19' - '13' + '17');
 console.log('19' - '13' + 17);
 console.log('123' < 57);
 console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+// LECTURE: Equality Operators: == vs. ===
+const numNeighbors = Number(
+  prompt('How many neighbor countries does your country have?')
+);
+if (numNeighbors === 1) {
+  console.log('Only 1 border!');
+} else if (numNeighbors > 1) {
+  console.log('More than 1 border');
+} else {
+  console.log('No borders');
+}
