d8a773c12a184d3c6a301a342c939705c80ad1b9
rizkirisjad <rizkirisjad@gmail.com>
2023-04-23 00:12:33 +0700

    menambah materi baru

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -57,3 +57,36 @@ const perIndonesia2 = percentageOfWorld2(275);
 const percChina2 = percentageOfWorld2(1441);
 const percUSA2 = percentageOfWorld2(332);
 console.log(perIndonesia2, percChina2, percUSA2);
+
+// LECTURE: Arrow Functions
+
+// 1. Recreate the last assignment, but this time create an arrow function called
+// 'percentageOfWorld3'
+
+const percentageOfWorld3 = (population) => (population / worldPopulation) * 100;
+
+const perIndonesia3 = percentageOfWorld3(275);
+const percChina3 = percentageOfWorld3(1441);
+const percUSA3 = percentageOfWorld3(332);
+console.log(perIndonesia3, percChina3, percUSA3);
+
+// LECTURE: Functions Calling Other Functions
+
+// 1. Create a function called 'describePopulation'. Use the function type you
+// like the most. This function takes in two arguments: 'country' and
+// 'population', and returns a string like this: 'China has 1441 million people,
+// which is about 18.2% of the world.'
+// 2. To calculate the percentage, 'describePopulation' call the
+// 'percentageOfWorld1' you created earlier
+// 3. Call 'describePopulation' with data for 3 countries of your choice
+
+const describePopulation = (country, population) => {
+  const percentage = percentageOfWorld1(population);
+  const description = `${country} has ${population} million
+people, which is about ${percentage}% of the world.`;
+  console.log(description);
+};
+
+describePopulation('Indonesia', 275);
+describePopulation('China', 1441);
+describePopulation('USA', 332);
